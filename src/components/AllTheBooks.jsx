768ad727b6738ca692cd737e084d09cb39adf679
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-01-30 16:41:01 +0100

    card component trasformato da funzione a classse

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,35 +1,38 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import horror from "../data/horror.json";
+import { Component } from "react";
 
-function BasicExample() {
-  return (
-    <div className="container">
-      <div className="row justify-content-between g-2">
-        {horror.map((film) => (
-          <Card
-            className="col-2"
-            key={film.id}
-            style={{ width: "9rem", height: "50vh" }}
-          >
-            <Card.Img
-              style={{ height: "20vh" }}
-              variant="top"
-              src={film.img}
-              alt={film.title}
-            />
-            <Card.Body className="row flex-column justify-content-between">
-              <Card.Title>{film.title}</Card.Title>
-              <Card.Text>
-                {film.category} <br /> ${film.price}
-              </Card.Text>
-              <Button variant="primary">Buy it</Button>
-            </Card.Body>
-          </Card>
-        ))}
+class BasicExample extends Component() {
+  render() {
+    return (
+      <div className="container">
+        <div className="row justify-content-between g-2 mx-2">
+          {horror.map((film) => (
+            <Card
+              className="col-2"
+              key={film.id}
+              style={{ width: "9rem", height: "60vh" }}
+            >
+              <Card.Img
+                style={{ height: "20vh" }}
+                variant="top"
+                src={film.img}
+                alt={film.title}
+              />
+              <Card.Body className="row flex-column justify-content-between">
+                <Card.Title>{film.title}</Card.Title>
+                <Card.Text>
+                  {film.category} <br /> ${film.price}
+                </Card.Text>
+                <Button variant="primary">Buy it</Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default BasicExample;
