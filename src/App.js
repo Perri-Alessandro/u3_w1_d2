c3c7d06355155182fca56bdf16b58d3b63aa634c
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-01-30 16:33:30 +0100

    ora rpovo a trasformare in classe il componente card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from "./components/MyNavComponent";
 import MyFooter from "./components/MyFooterComponent";
 import Alert from "./components/WelcomeComponent";
+import BasicExample from "./components/AllTheBooks";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <MyNav title="Magic" />
       </header>
       <Alert></Alert>
+      <BasicExample></BasicExample>
       <MyFooter tit1="NOI" tit2="I NOSTRI PRODOTTI" tit3="HOME"></MyFooter>
     </div>
   );
---
+++ src/components/AllTheBooks.jsx
@@ -0,0 +1,35 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import horror from "../data/horror.json";
+
+function BasicExample() {
+  return (
+    <div className="container">
+      <div className="row justify-content-between g-2">
+        {horror.map((film) => (
+          <Card
+            className="col-2"
+            key={film.id}
+            style={{ width: "9rem", height: "50vh" }}
+          >
+            <Card.Img
+              style={{ height: "20vh" }}
+              variant="top"
+              src={film.img}
+              alt={film.title}
+            />
+            <Card.Body className="row flex-column justify-content-between">
+              <Card.Title>{film.title}</Card.Title>
+              <Card.Text>
+                {film.category} <br /> ${film.price}
+              </Card.Text>
+              <Button variant="primary">Buy it</Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BasicExample;
